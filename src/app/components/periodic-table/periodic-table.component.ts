6d4acf9bfee5b9147c6d9d0a3d002fda6ce293ac
Jakub <devcobbier@gmail.com>
2024-09-16 15:33:21 +0200

    implemendted filtering data

--- src/app/components/periodic-table/periodic-table.component.ts
+++ src/app/components/periodic-table/periodic-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -7,6 +7,8 @@ import { ReactiveFormsModule, FormsModule, FormControl } from '@angular/forms';
 import { PeriodicTableService } from './periodic-table.service';
 import { PeriodicElement } from './periodic-table.model';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { debounceTime } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-periodic-table',
@@ -35,14 +37,48 @@ export class PeriodicTableComponent implements OnInit {
   dataSource: PeriodicElement[] = [];
   filteredData: PeriodicElement[] = [];
   isLoading = true;
+  private destroyerRef = inject(DestroyRef);
 
   constructor(private periodicTableService: PeriodicTableService) {}
 
   ngOnInit(): void {
-    this.periodicTableService.getElements().subscribe((data) => {
-      this.dataSource = data;
-      this.filteredData = data;
-      this.isLoading = false;
-    });
+    this.loadData();
+    this.initializeFilter();
+  }
+
+  private loadData(): void {
+    this.isLoading = true;
+    this.periodicTableService
+      .getElements()
+      .pipe(takeUntilDestroyed(this.destroyerRef))
+      .subscribe((data) => {
+        this.dataSource = data;
+        this.filteredData = data;
+        this.isLoading = false;
+      });
+  }
+
+  private initializeFilter(): void {
+    this.filterControl.valueChanges
+      .pipe(debounceTime(2000), takeUntilDestroyed(this.destroyerRef))
+      .subscribe((value) => {
+        const filterValue = value ? value.trim().toLowerCase() : '';
+        this.applyFilter(filterValue);
+      });
+  }
+
+  applyFilter(filter: string): void {
+    if (!filter) {
+      this.filteredData = this.dataSource;
+      return;
+    }
+
+    this.filteredData = this.dataSource.filter(
+      (element) =>
+        element.name.toLowerCase().includes(filter) ||
+        element.symbol.toLowerCase().includes(filter) ||
+        element.weight.toString().includes(filter) ||
+        element.position.toString().includes(filter)
+    );
   }
 }
